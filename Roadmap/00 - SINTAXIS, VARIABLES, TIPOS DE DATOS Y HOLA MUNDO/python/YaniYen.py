07207cd480b93cc59e5793cffdca22f12867c3f1
YaniYen <yenleeyan@gmail.com>
2024-11-05 11:18:21 +0845

    Create YaniYen.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/YaniYen.py
@@ -0,0 +1,27 @@
+#https://python.org
+
+#Comentario en una linea
+
+""" 
+Esto tambien nos indica que 
+es un comentario en varias lineas
+"""
+
+'''
+Esto tambien nos indica que 
+es un comentario en varias lineas
+'''
+
+my_variable = "Esta es mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "Mi Constante"
+
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_string = "Mi candena de texto"
+my_other_string = 'Mi otra cadena de texto'
+
+print("¡Hola, Mundo and Python!")
